708c6f03acfeacdb0c25e8b4a3a1ab587eaa5d66
lecovich <leclecovich@gmail.com>
2022-09-09 23:02:05 +0300

    Adding initial files.

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+from datetime import datetime
+
+with open('input.json', 'r') as fp:
+    d = json.load(fp)
+
+currencies = d[0]['currencies']
+currency_date = datetime.fromisoformat(d[0]['date'].replace('Z', ''))
+
+for currency in currencies:
+    if currency['code'] == 'USD':
+        rate = currency['rate']
+        print(rate, currency_date.isoweekday())
