5800e85a50db222203ebf955b69962495bcc3040
lecovich <leclecovich@gmail.com>
2022-09-13 09:26:53 +0300

    Saving result to `output.json`

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ with open('input.json', 'r') as fp:
 currencies = d[0]['currencies']
 currency_date = datetime.fromisoformat(d[0]['date'].replace('Z', ''))
 
+result = {}
+
 for currency in currencies:
     if currency['code'] == 'USD':
         rate = currency['rate']
-        print(f'{currency_date.date()}: {rate}')
+        result = {
+            'date': currency_date.isoformat(),
+            'rate': rate,
+        }
+
+with open('output.json', 'w') as fp:
+    json.dump(result, fp)
